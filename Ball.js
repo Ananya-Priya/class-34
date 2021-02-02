d374f92853f5b501c57083e007ac93636c2d62cd
Ananya-Priya <76117412+Ananya-Priya@users.noreply.github.com>
2021-02-02 21:03:44 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,28 @@
+class Ball
+{
+    constructor(x,y,radius)
+    {
+        var options=
+        {
+                isStatic : false,
+                restitution:0.04,
+                friction:0.3,
+                density:1.2
+        }
+
+        this.body=Bodies.circle(x,y,radius,options);
+        this.radius=radius
+        
+
+        World.add(world,this.body);
+
+    }
+
+    display()
+    {
+        ellipseMode(RADIUS);
+        fill("pink");
+        ellipse(this.body.position.x,this.body.position.y, this.radius  ,  this.radius );
+      
+    }
+}
